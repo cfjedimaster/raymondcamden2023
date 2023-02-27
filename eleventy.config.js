0447dd32b18e58b0687928cd629035794bf9854d
Raymond Camden <raymondcamden@gmail.com>
2023-02-27 16:41:27 -0600

    link headers

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,8 @@ const { getPosts } = require('./config/collections/index.js');
 const { extractExcerpt } = require('./config/shortcodes/index.js');
 const { ageInDays } = require('./config/filters/index.js');
 
+const markdownIt = require('markdown-it');
+const markdownItAnchor = require('markdown-it-anchor');
 
 module.exports = function(eleventyConfig) {
 
@@ -13,6 +15,27 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addShortcode('excerpt', post => extractExcerpt(post));
 
 	eleventyConfig.addFilter('ageInDays', ageInDays);
+
+	/*
+	Specifically for auto link headers.
+	https://rhianvanesch.com/posts/2021/02/09/adding-heading-anchor-links-to-an-eleventy-site/
+	*/
+	const markdownItOptions = {
+		html: true,
+	}
+
+	// Options for the `markdown-it-anchor` library
+	const markdownItAnchorOptions = {
+		permalink: true, 
+		permalinkSymbol: '#'
+	}
+
+	const markdownLib = markdownIt(markdownItOptions).use(
+		markdownItAnchor,
+		markdownItAnchorOptions
+	);
+
+	eleventyConfig.setLibrary("md", markdownLib);
 
 	return {
 		dir: {
