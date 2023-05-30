47193cad9478bc0a29ed33e3edef016387d06d95
Raymond Camden <raymondcamden@gmail.com>
2023-05-30 15:23:33 -0500

    new part to my now page

---
+++ src/_data/untappd_beers.js
@@ -0,0 +1,26 @@
+
+const CLIENT_ID = process.env.UNTAPPD_CLIENT_ID;
+const CLIENT_SECRET = process.env.UNTAPPD_CLIENT_SECRET;
+const fetch = require('node-fetch');
+
+module.exports = async function() {
+
+    let resp = await fetch(`https://api.untappd.com/v4/user/checkins/cfjedimaster?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&limit=10`, {
+      headers: {
+        'user-agent':'MyAgentBringsAllTheBoysToTheYard'
+      }
+    });
+    let data = await resp.json();
+    return data.response.checkins.items.map(b => {
+      return {
+          created: b.created_at, 
+          rating: b.rating_score, 
+          name: b.beer.beer_name, 
+          label: b.beer.beer_label, 
+          style: b.beer.beer_style, 
+          abv: b.beer.beer_abv,
+          brewery: b.brewery.brewery_name
+      }
+    });
+	
+};
