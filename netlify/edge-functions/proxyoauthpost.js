e56ed238f2b0fa278d4063c8c4bd67c5ac9f8903
Raymond Camden <raymondcamden@gmail.com>
2024-04-03 12:04:21 -0500

    test blob

--- netlify/edge-functions/proxyoauthpost.js
+++ netlify/edge-functions/proxyoauthpost.js
@@ -1,8 +1,28 @@
+import { getStore } from "@netlify/blobs";
 
 export default async (request, context) => {
-	console.log(context);
-	console.log(request.headers);
-	return new Response("Hello world");
+	/*
+	let ref = request.headers.get('referer');
+	let ua = request.headers.get('user-agent');
+	
+	*/;
+
+	const tracker = getStore('tracker');
+
+	let packet = {
+		ref: request.headers.get('referer'),
+		ua: request.headers.get('user-agent'),
+		ip: context.ip, 
+		geo: context.geo
+	};
+
+	let log = await tracker.get('log', { type:'json' });
+	if(!log) log = [];
+	log.push(packet);
+	console.log(log);
+	await tracker.setJSON('log', log);
+
+	return;
 }
 
-export const config = { path: "/test" };
+export const config = { path: "/2013/04/03/ColdFusion-and-OAuth-Part-2-Facebook" };
