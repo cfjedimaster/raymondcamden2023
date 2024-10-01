80e1244f3d7ee74e012da5d5c54a6bcb704ef0ae
Raymond Camden <raymondcamden@gmail.com>
2024-10-01 16:08:49 -0700

    fine again2

--- netlify/functions/deploy-succeeded/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded/deploy-succeeded.js
@@ -74,6 +74,8 @@ export default async (req, context) => {
     New logic to get the event body
     */
     console.log('NEW BUILD');
+    console.log('req.body', req.body);
+
     let event = JSON.parse(req.body).event;
     console.log('NEW EVENT', event);
 
